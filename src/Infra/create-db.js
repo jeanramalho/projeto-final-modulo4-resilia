c20940d64bacba150c5311a94e30336c955b78ed
jeanramalho <jeanramalho.dev@gmail.com>
2021-11-25 19:10:48 -0300

    Tenta corrigir bug de banco de dados

--- src/Infra/create-db.js
+++ src/Infra/create-db.js
@@ -1,8 +1,8 @@
 const sqlite3 = require('sqlite3').verbose()
-const path = require(path)
+const path = require('path')
 const caminhoArq = path.resolve(__dirname,'database.db')
 const db = new sqlite3.Database(caminhoArq)
-
+const teste =  console.log('testando')
 
 
 const USUARIOS_SCHEMA = `
@@ -13,31 +13,31 @@ CREATE TABLE IF NOT EXISTS "USUARIOS" (
     "email" varchar(70),
     "endereco" varchar(100),
     "senha" varchar(15)
-);`;
+)`
 
 const ADD_USUARIO_TESTE = `
-INSERT INTO USUARIOS (ID, NOME, TELEFONE, EMAIL, ENDERECO, SENHA)
+INSERT INTO USUARIOS (id, nome, telefone, email, endereco, senha)
 VALUES 
-    (1, 'Eugênio Oliveira','3522-0000', 'eugenio.oliveira@bol.com.br', 'Rua Belgica,15', '*******'),
-    
-`;
+    (1, 'Eugênio Oliveira','3522-0000', 'eugenio.oliveira@bol.com.br', 'Rua Belgica,15', '12345678')    
+`
 
 function criaTabelaUsr() {
-    db.run(USUARIOS_SCHEMA, (error)=> {
-       if (error) console.log("Erro ao criar tabela de usuários");
-    });
+    console.log('tentando criar tabela')
+    db.run(USUARIOS_SCHEMA, (error) => {
+       if (error) console.log("Erro ao criar tabela de usuários")
+    })
 }
 
 function populaTabelaUser() {
     db.run(ADD_USUARIO_TESTE, (error)=> {
-       if (error) console.log("Erro ao popular tabela de usuários");
-    });
-};
+       if (error) console.log("Erro ao popular tabela de usuários")
+    })
+}
 
 db.serialize(() => {
     criaTabelaUsr();
     populaTabelaUser();
-});
+})
 
 
 
